0b9917f4736b6f2ae754f3ce5794b909d8eca83d
Michelle Olivieri <michelleo72@gmail.com>
2018-03-26 16:50:04 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(".","!","?").map {|element| element.count_sentences.compact}
+    self.map {|element| element.split.count_sentences.compact}
   end
   end
 end
