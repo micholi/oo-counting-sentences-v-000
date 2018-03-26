55dfa3f7f3596fb469207626606a1c98d8aa6c92
Michelle Olivieri <michelleo72@gmail.com>
2018-03-26 17:27:43 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-  self.split(/[.!?]/).map {|string| delete!.string(&:empty?)}.size
-
+  #self.split(/[.!?]/).map {|string| delete!.string(&:empty?)}.size
+  self.split(/[.!?]/).reject {|string| string.nil? || string = " "}.size
   end
 end
